0a83f3e186ebeed9db7f0362c09a667d731b19bb
Thomas Fortin <18682135+thomas-fortin@users.noreply.github.com>
2020-07-12 14:28:21 +0200

    Add some Tailwind and Stylelint configs

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,7 +2,17 @@ module.exports = {
   extends: [
     'stylelint-config-standard'
   ],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
-  rules: {}
-}
+  rules: {
+    'at-rule-no-unknown': [true, {
+      ignoreAtRules: [
+        'tailwind',
+        'apply',
+        'variants',
+        'responsive',
+        'screen'
+      ]
+    }],
+    'declaration-block-trailing-semicolon': null,
+    'no-descending-specificity': null
+  }
+};
---
+++ tailwind.config.js
@@ -0,0 +1,68 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+const { colors } = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  theme: {
+    colors: {
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      blue: {
+        '100': '#90dfff',
+        '200': '#68d4ff',
+        '300': '#00b7ff',
+        '400': '#04Aef0',
+        '500': '#08a5e1',
+        '600': '#0795d3',
+        '700': '#0586c5',
+        '800': '#0474ab',
+        '900': '#056492'
+      },
+      green: {
+        '100': '#5eff9e',
+        '200': '#29f57a',
+        '300': '#25e471',
+        '400': '#29d970',
+        '500': '#2dcf6e',
+        '600': '#30c46c',
+        '700': '#33ba6a',
+        '800': '#24a95a',
+        '900': '#0c843d'
+      },
+      primary: colors.blue[500],
+      secondary: colors.green[500],
+      grayish: colors.gray[700]
+    },
+    extend: {
+      fontSize: {
+        '7xl': '5rem',
+        '8xl': '6rem',
+        '9xl': '8rem',
+        '10xl': '10rem',
+        '11xl': '12rem',
+        '12xl': '14rem'
+      },
+      lineHeight: {
+        'negative': '0.75',
+        '12': '3rem'
+      },
+      inset: {
+        '8': '8em',
+        '10': '10em'
+      },
+      opacity: {
+        '90': '0.9'
+      },
+      borderWidth: {
+        '1': '1px'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
