ccfd1130f1b36f00c3727447270835afc90d56a2
Thomas Fortin <18682135+thomas-fortin@users.noreply.github.com>
2020-07-12 14:27:13 +0200

    Add some ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,62 @@ module.exports = {
   ],
   plugins: [
   ],
-  // add your custom rules here
-  rules: {}
-}
+  rules: {
+    'object-curly-spacing': [
+      'error',
+      'always'
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi': [
+      'error',
+      'always'
+    ],
+    'spaced-comment': [
+      'error',
+      'always'
+    ],
+    'prefer-template': 'error',
+    'quotes': [
+      'error',
+      'single',
+      {
+        'allowTemplateLiterals': true
+      }
+    ],
+    'indent': [
+      'error',
+      2
+    ],
+    'eol-last': [
+      'error',
+      'always'
+    ],
+    'arrow-spacing': 'error',
+    'arrow-parens': [
+      'error',
+      'always'
+    ],
+    'max-len': [
+      'error',
+      {
+        'code': 160
+      }
+    ],
+    'no-trailing-spaces': 'error',
+    'no-multiple-empty-lines': 'error',
+    'one-var-declaration-per-line': [
+      'error',
+      'always'
+    ],
+    'switch-colon-spacing': 'error',
+    'space-before-function-paren': [
+      'error',
+      'never'
+    ],
+    'quote-props': [
+      'error',
+      'consistent'
+    ]
+  }
+};
