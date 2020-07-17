e2b5e93cf5427142d2ca4383f682f8d20daaff89
Thomas Fortin <18682135+thomas-fortin@users.noreply.github.com>
2020-07-17 12:08:26 +0200

    Create the Skill component

---
+++ components/shared/Skill.vue
@@ -0,0 +1,24 @@
+<template>
+  <span
+    class="inline-block rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-3 text-white"
+    :class="'bg-' + color"
+  >
+    {{ name }}
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'Skill',
+  props: {
+    name: {
+      type: String,
+      default: 'Unknown'
+    },
+    color: {
+      type: String,
+      default: 'primary'
+    }
+  }
+};
+</script>
