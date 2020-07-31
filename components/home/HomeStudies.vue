edc0ab0b423140cc85c5b582015fd269af3bd726
Thomas Fortin <18682135+thomas-fortin@users.noreply.github.com>
2020-07-31 20:27:19 +0200

    Create the HomeStudies component and prepare it for the following StudyCards

---
+++ components/home/HomeStudies.vue
@@ -0,0 +1,34 @@
+<template>
+  <section id="homeStudies" class="bg-gray-100">
+    <div class="container mx-auto py-5">
+      <h3 class="section-title">
+        {{ $t('homeStudies.title') }}
+      </h3>
+      <div class="homeStudies__content mt-8">
+        <p class="text-center">
+        {{ $t('homeStudies.catchPhrase') }}
+        </p>
+        <div class="mt-16 flex flex-wrap justify-center studies">
+          <!-- <template v-if="studies">
+            <StudyCard v-for="study in studies" :key="study.id" :study="study" />
+          </template> -->
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+// import { mapGetters } from 'vuex';
+
+// export default {
+//   computed: {
+//     ...mapGetters('studies', {
+//       'studies': 'getStudies'
+//     })
+//   },
+//   created() {
+//     this.$store.dispatch('studies/fetchStudies');
+//   }
+// };
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,21 @@
     <HomeTop />
     <HomeExperiences />
     <HomeCallToAction />
+    <HomeStudies />
   </div>
 </template>
 
 <script>
 import HomeTop from '@/components/home/HomeTop';
 import HomeExperiences from '@/components/home/HomeExperiences';
+import HomeStudies from '@/components/home/HomeStudies';
 import HomeCallToAction from '@/components/home/HomeCallToAction';
 
 export default {
   components: {
     HomeTop,
     HomeExperiences,
+    HomeStudies,
     HomeCallToAction
   }
 };
