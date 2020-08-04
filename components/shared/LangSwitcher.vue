ee03db9e1fb6fe067de3e5d19b15983fc37276ca
Thomas Fortin <18682135+thomas-fortin@users.noreply.github.com>
2020-08-04 22:34:31 +0200

    Create the LangSwitcher component

---
+++ components/shared/LangSwitcher.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div
+      v-for="locale in availableLocales"
+      :key="locale.code"
+      class="cursor-pointer"
+      @click="switchLocale(locale)"
+    >
+      <img :src="require(`@/assets/img/flags/${locale.code}.svg`)" :alt="locale.name">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    availableLocales() {
+      return this.$i18n.locales.filter((i) => {
+        return i.code !== this.$i18n.locale;
+      });
+    }
+  },
+  methods: {
+    switchLocale(locale) {
+      this.$router.push({ path: this.switchLocalePath(locale.code) });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 30px;
+  height: 30px;
+}
+</style>
