9b00148fa9f832c7782a69eca9ce15cdc12cd178
Thomas Fortin <18682135+thomas-fortin@users.noreply.github.com>
2020-08-04 22:36:17 +0200

    Integration of the LangSwitcher on the header

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,9 +1,19 @@
 <template>
-  <header class="fixed w-full z-20" role="banner">
-    <div class="container mx-auto">
+  <header class="absolute w-full z-20" role="banner">
+    <div class="container mx-auto flex justify-between py-3">
       <nuxt-link :to="localePath('index')">
         <span class="text-white text-2xl">Thomas Fortin</span>
       </nuxt-link>
+      <LangSwitcher />
     </div>
   </header>
 </template>
+
+<script>
+import LangSwitcher from '@/components/shared/LangSwitcher';
+export default {
+  components: {
+    LangSwitcher
+  }
+};
+</script>
