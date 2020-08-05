0aafddba64ba03ee91f3366e54217898dda4e30e
Thomas Fortin <18682135+thomas-fortin@users.noreply.github.com>
2020-08-05 23:01:55 +0200

    Add meta infos on the index page

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,10 +15,10 @@ export default {
   */
   head: {
     title: process.env.npm_package_name || '',
+    titleTemplate: `%s | ${process.env.SITE_NAME}`,
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,7 @@ import HomeTop from '@/components/home/HomeTop';
 import HomeExperiences from '@/components/home/HomeExperiences';
 import HomeStudies from '@/components/home/HomeStudies';
 import HomeCallToAction from '@/components/home/HomeCallToAction';
+import ogImage from '@/assets/img/photo_profil.jpg';
 
 export default {
   components: {
@@ -22,7 +23,22 @@ export default {
   },
   head() {
     return {
-      title: `${this.$t('home')} | ${process.env.SITE_NAME}`
+      title: `${this.$t('home')}`,
+      meta: [
+        { hid: 'title', name: 'title', content: `${this.$t('home')} | ${process.env.SITE_NAME}` },
+        { hid: 'description', name: 'description', content: `${this.$t('description')}` },
+        { hid: 'og_title', property: 'og:title', content: `${this.$t('home')} | ${process.env.SITE_NAME}` },
+        { hid: 'og_sitename', property: 'og:site_name', content: `${process.env.SITE_NAME}` },
+        { hid: 'og_type', property: 'og:type', content: `website` },
+        { hid: 'og_url', property: 'og:url', content: `https://www.thomasfortin.fr` },
+        { hid: 'og_image', property: 'og:image', content: `https://www.thomasfortin.fr${ogImage}` },
+        { hid: 'og_image_url', property: 'og:image:url', content: `https://www.thomasfortin.fr${ogImage}` },
+        { hid: 'twitter_title', name: 'twitter:title', content: `${this.$t('home')} | ${process.env.SITE_NAME}` },
+        { hid: 'twitter_description', name: 'twitter:description', content: `${this.$t('description')}` },
+        { hid: 'twitter_creatorid', name: 'twitter:creator:id', content: `tfortin_pro` },
+        { hid: 'twitter_url', name: 'twitter:url', content: `https://www.thomasfortin.fr` },
+        { hid: 'twitter_card', name: 'twitter:card', content: `summary_large_image` }
+      ]
     };
   }
 };
