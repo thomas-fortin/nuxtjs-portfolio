f167f175fd87700fe2b672e82682aca4ab46a46c
Thomas Fortin <18682135+thomas-fortin@users.noreply.github.com>
2020-08-08 20:31:11 +0200

    Add a formatter when getting dates on the experiences store module

--- store/experiences.js
+++ store/experiences.js
@@ -43,9 +43,23 @@ export const actions = {
   },
   async fetchOneExperience({ commit }, slug) {
     commit('GET_EXPERIENCE', {});
-    const snapshot = await this.$fireStore.collection(`experiences_${this.app.i18n.locale}`).where('slug', '==', slug).get();
+    const currentLocale = this.app.i18n.locales.find((i) => { return i.code === this.$i18n.locale; });
+    const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
 
-    const experiences = snapshot.docChanges().map(({ doc }) => doc.data());
+    const snapshot = await this.$fireStore.collection(`experiences_${currentLocale.code}`).where('slug', '==', slug).get();
+
+    const experiences = snapshot.docChanges()
+      .map(({ doc }) => ({
+        id: doc.id,
+        title: doc.data().title,
+        company: doc.data().company,
+        place: doc.data().place,
+        type: doc.data().type,
+        skills: doc.data().skills,
+        slug: doc.data().slug,
+        start: new Date(doc.data().start.seconds * 1000).toLocaleDateString(currentLocale.iso, dateOptions),
+        end: new Date(doc.data().end.seconds * 1000).toLocaleDateString(currentLocale.iso, dateOptions)
+      }));
     const matchingExperience = experiences[0];
 
     commit('GET_EXPERIENCE', matchingExperience);
